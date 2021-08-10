00e285214ffecdf9bfb38fa1d65273babe4e599a
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-08-10 18:54:44 +0530

    created NewsViewModel

--- app/src/main/java/com/gmail/apigeoneer/citywire/NewsActivity.kt
+++ app/src/main/java/com/gmail/apigeoneer/citywire/NewsActivity.kt
@@ -3,37 +3,30 @@ package com.gmail.apigeoneer.citywire
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.gmail.apigeoneer.citywire.api.NewsService
 import com.gmail.apigeoneer.citywire.data.models.Article
+import com.gmail.apigeoneer.citywire.databinding.ActivityNewsBinding
+import com.gmail.apigeoneer.citywire.viewmodels.NewsViewModel
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class NewsActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityNewsBinding
+    private val _viewModel by viewModels<NewsViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_news)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_news)
 
-        getNewsArticles()
+        binding
 
-    }
+        _viewModel.getNewsArticles()
 
-    private fun getNewsArticles() {
-        // call the http query fun in the api interface via the singleton
-        val allArticles = NewsService.newsApiService.getNews("gorakhpur",
-            "2021-8-3", "2021-8-9","popularity", getString(R.string.API_KEY))
-
-        allArticles.enqueue(object: Callback<Article> {
-            override fun onResponse(call: Call<Article>, response: Response<Article>) {
-                Log.d(TAG, "::::::: Api Response: $response :::::::")
-            }
-
-            override fun onFailure(call: Call<Article>, t: Throwable) {
-                Log.d(TAG, "::::::: No Api Response: $t :::::::")
-            }
-
-        })
     }
 
     companion object {
