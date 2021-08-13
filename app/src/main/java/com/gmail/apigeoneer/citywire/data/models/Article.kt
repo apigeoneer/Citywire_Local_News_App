2b3d0cc387932747c7243d4db2b869a2412fbc41
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-08-13 23:22:14 +0530

    added Room dependencies & converted Article data class to Room Entity

--- app/src/main/java/com/gmail/apigeoneer/citywire/data/models/Article.kt
+++ app/src/main/java/com/gmail/apigeoneer/citywire/data/models/Article.kt
@@ -1,10 +1,15 @@
 package com.gmail.apigeoneer.citywire.data.models
 
 import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 
+@Entity(tableName = "articles")
 @Parcelize
 data class Article(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0L,                           // Created to use as the primary key
     val source: Source,
     val author: String,
     val title: String,
